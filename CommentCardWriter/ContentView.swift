ca631682dc6e20e08855e5d0f79723f4b125aafd
Sritej Tummuru <tummuru.s@etoncollege.org.uk>
2023-02-08 10:32:30 +0000

    Added preference feature for generating comments.

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -9,22 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var comment = Comment()
-    @State private var textFieldOpacity = 0.0
+    @State private var preference = 5.0
+    @State private var subject = "subject"
     
     var body: some View {
+        
         VStack(alignment: .center, spacing: 20) {
             TextField(
-                "Comment for subject:",
-                text: $comment.text
-            ).opacity(textFieldOpacity)
-            
-            Button(
-                "Generate Comment",
-                action: {
-                    comment.generateRandomComment()
-                    textFieldOpacity = 1
+                "Enter subject here...",
+                text: $subject,
+                onEditingChanged:{ _ in
+                    comment.preference = preference
+                    comment.generatePreferenceComment()
                 }
             )
+                .border(.secondary)
+                
+            Slider(
+                value: $preference,
+                in: 0...10,
+                onEditingChanged: { _ in
+                    comment.preference = preference
+                    comment.generatePreferenceComment()
+                }
+            )
+            Text(comment.text)
         }
     }
 }
--- Models/Comment.swift
+++ Models/Comment.swift
@@ -8,10 +8,23 @@
 import Foundation
 
 class Comment: ObservableObject {
-    @Published var text = "This is a comment."
+    @Published var text = ""
+    var preference = 5.0
+    var subject = ""
+    
     var possibleComments = ["Comment1", "Comment2", "Comment3", "Comment4", "Comment5"]
     
     func generateRandomComment() {
         self.text = self.possibleComments[Int.random(in: 0..<5)]
+    }
+    
+    func generatePreferenceComment() {
+        if preference > 7 {
+            text = "I am loving \(subject)!"
+        } else if preference <= 7 && preference >= 3 {
+            text = "I think \(subject) is ok."
+        } else {
+            text = "I hate \(subject)!"
+        }
     }
 }
